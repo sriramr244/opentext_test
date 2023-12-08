982908e9913c5a788ad7c95872cd443c9a532cc3
Sriram R <sriramr244@gmail.com>
2023-12-08 15:31:12 -0500

    changed the issue in unit test

--- test/test_dre_random_generator.py
+++ test/test_dre_random_generator.py
@@ -8,7 +8,7 @@ class TestGenerateStoreRandomNumber(unittest.TestCase):
     def test_file_creation(self):
         """Test if the HDF5 file is created."""
         test_file = "test_random_numbers.hdf5"
-        gen_and_store_random_numbers(42, 100, test_file)
+        gen_and_store_random_numbers(42, 100, test_file, 100)
         self.assertTrue(os.path.exists(test_file))
         os.remove(test_file)
 
@@ -16,7 +16,7 @@ class TestGenerateStoreRandomNumber(unittest.TestCase):
         """Test if the file contains the correct number of elements."""
         test_file = "test_random_numbers.hdf5"
         length = 100
-        gen_and_store_random_numbers(42, length, test_file)
+        gen_and_store_random_numbers(42, length, test_file, 100)
         with h5py.File(test_file, "r") as hdf:
             dset = hdf["random_numbers"]
             self.assertEqual(len(dset), length)
